38d64584ae175b3d9b7df350cbb4699a19229e74
Angelo <97480357+aerdni99@users.noreply.github.com>
2022-10-24 17:20:48 -0400

    Revert "adi19 codebase"

--- implementation-3/src/Server.h
+++
@@ -1,7 +0,0 @@
-#ifndef SERVER_H_INCLUDED
-#define SERVER_H_INCLUDED
-
-int prepareSocket(const char * listenAddr, const char * port, bool isServer);
-int establishConn(int& cliSock, int& listenSock, int& clientSock, bool& isSrv, bool& isCli);
-
-#endif // SERVER_H_INCLUDED
